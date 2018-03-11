cd9a47776574e1ff1b5f6b2cd5cc38171af2dba0
Will Spencer <will.e.spencer@gmail.com>
2018-03-11 07:12:20 -0400

    Started JSON File
    Added options json file for final intent

--- helpers/feeling.js
+++ helpers/feeling.js
@@ -4,7 +4,7 @@ module.exports = (response) => {
 	'scared':['scared','panic','fearful','terrified','nervous','distressed','afraid','panicky','panicked','shaken','petrified','anxious','anxiety'],
 	'suicidal':['suicidal','kill my self','kms','suicide','destructive','very depressed','super depressed','so depressed']}
 	const suggestions = {'sad': {'short-term':['See cute animal pictures', 'read a book','go on social media','watch a movie/tv show','play video games','message a good friend', 'take a nap', 'find a quiet/isolated place','write a journal'], 'long-term':['Set goals', 'keep a healthy diet', 'have a regular exercise routine', 'sleep more', 'have some (but not too many!) responsibilities on your plate']},
-	'angry':{'short-term':['Take deep breaths','stay positive','find someone to rant to', 'find funny things that will make you laugh'], 'long-term':['Talk to a therapist/professional', 'take anger management courses']},
+	'angry':{'short-term':['Take deep breaths','stay positive','find someone to rant to', 'find funny things that will make you laugh'], 'long-term':['Talk to a therapist or professional', 'take anger management courses']},
 	'scared':{'short-term':['Stop over-thinking','think of a positive memory','take a walk', 'find someone to talk to', 'calm music/nature sounds'], 'physically active':['Complete breathing exercises', 'do relaxing muscle exercises'], 'long-term':['Avoid or limit caffeine', 'get active', 'add more omega-3 fats to your diet', 'get enough sleep', 'talk to a therapist']},
 	'suicidal':{'short-term':['call 1-800-273-8255 if you have thoughts about death or self-harm', 'talk to a trusted professional doctor or counselor']}}
 
