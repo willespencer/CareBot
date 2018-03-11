e2fee00a5d650f87cb42f6f2da631e19c80ddc19
Will Spencer <will.e.spencer@gmail.com>
2018-03-10 22:02:38 -0500

    Feeling intent setup
    Set up use of fullfilment for feeling intent

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -14,16 +14,24 @@ const sendTextMessage = (senderId, text) => {
 module.exports = (event) => {
     const senderId = event.sender.id;
     const message = event.message.text;
-    //const apiAiClient= require('actions-on-google').apiAiClient;
 
     var apiai = require('apiai');
-
     var apiAiClient = apiai("0fe4f6ae45044b02bb9c6dee50be9f5d");
 
     const apiaiSession = apiAiClient.textRequest(message, { sessionId: 'crowdbotics_bot' });
     apiaiSession.on('response', (response) => {
-        const result = response.result.fulfillment.speech;
-        sendTextMessage(senderId, result);
+      var result;
+      if(response.result.parameters.Feeling)
+      {
+        const feelingIntent = require('feelings');
+        result = feelingIntent(response);
+      }
+      else
+      {
+        result = response.result.fulfillment.speech;
+      }
+
+      sendTextMessage(senderId, result);
     });
     apiaiSession.on('error', error => console.log(error));
     apiaiSession.end();
