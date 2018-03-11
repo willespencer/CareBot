a4fa7dff8754b187405cae769f35731c73db3be6
Will Spencer <will.e.spencer@gmail.com>
2018-03-10 20:42:07 -0500

    Chatbot set up
    Completed Chatbot setup, is functional

---
+++ helpers/processMessage.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const sendTextMessage = (senderId, text) => {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: 'EAAbQWJJiLPkBAMbMbDjBz2fSUvPmOgpGKF3FqfGpAB9cqs6JUMiPwHeKPtWxgKZCZA5nYSaCGu7SfczqWYDpAeY2gJ3WX3SQeTHqG32NNn4SmGv6ZB3IWdhom24scy4ZC7BPb9ZBKUaNdWOOEaGxmnZBfSTVdHpHr8En5t6nCMZAAZDZD' },
+        method: 'POST',
+        json: {
+            recipient: { id: senderId },
+            message: { text },
+        }
+    });
+};
+
+module.exports = (event) => {
+    const senderId = event.sender.id;
+    const message = event.message.text;
+    //const apiAiClient= require('actions-on-google').apiAiClient;
+
+    var apiai = require('apiai');
+
+    var apiAiClient = apiai("0fe4f6ae45044b02bb9c6dee50be9f5d");
+
+    const apiaiSession = apiAiClient.textRequest(message, { sessionId: 'crowdbotics_bot' });
+    apiaiSession.on('response', (response) => {
+        const result = response.result.fulfillment.speech;
+        sendTextMessage(senderId, result);
+    });
+    apiaiSession.on('error', error => console.log(error));
+    apiaiSession.end();
+};
